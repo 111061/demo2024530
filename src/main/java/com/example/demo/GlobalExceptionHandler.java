cb5eb50c764bdbbe7c1b59ecaaa0f9830afcd080
111061 <a10443087@gmail.com>
2024-05-31 10:43:34 +0900

    更改

--- src/main/java/com/example/demo/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/GlobalExceptionHandler.java
@@ -1,10 +1,10 @@
 package com.example.demo;
 
-import org.springframework.http.HttpStatus;
+import
+org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
